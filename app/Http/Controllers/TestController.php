ac18244ef31d5756e66031d6c37953940cb0164d
CaoAnhVu141 <161787991+CaoAnhVu141@users.noreply.github.com>
2024-10-03 14:30:55 +0700

    demo

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -10,6 +10,6 @@ class TestController extends Controller
 
     public function testcai()
     {
-        return view('Front-end-Admin.menu.update');
+        return view('Front-end-Admin.oders.index');
     }
 }
