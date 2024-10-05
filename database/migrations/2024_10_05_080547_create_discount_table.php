11d5389bba9bb2f3cc3baa13764c4c352d5eedbd
CaoAnhVu141 <161787991+CaoAnhVu141@users.noreply.github.com>
2024-10-05 16:14:17 +0700

    commit by caoanhvu

---
+++ database/migrations/2024_10_05_080547_create_discount_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('discount', function (Blueprint $table) {
+            $table->bigIncrements('id_discount');
+            $table->string('code');
+            $table->string('describe')->nullable();
+            $table->datetime('start_day');
+            $table->datetime('end_day');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('discount');
+    }
+};
