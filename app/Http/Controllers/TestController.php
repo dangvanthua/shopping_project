2e5a4614f0056184fe0d97fac6e4c03b6913c1fe
CaoAnhVu141 <161787991+CaoAnhVu141@users.noreply.github.com>
2024-10-19 02:38:48 +0700

    commit by caoanhvu

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -10,6 +10,6 @@ class TestController extends Controller
 
     public function testcai()
     {
-        return view('Front-end-Shopping.favorite_product');
+        return view('Front-end-Shopping.404');
     }
 }
