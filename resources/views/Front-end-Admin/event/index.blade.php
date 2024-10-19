1a3fefbe7b8fcdec79f59d0d3da6e185771e261b
dangvanthua <thuandang021102@gmail.com>
2024-10-19 14:15:35 +0700

    update pagination

--- resources/views/Front-end-Admin/event/index.blade.php
+++ resources/views/Front-end-Admin/event/index.blade.php
@@ -11,9 +11,7 @@
             <li class="active">list</li>
         </ol>
     </section>
-    <!-- Main content -->
     <section class="content">
-        <!-- Small boxes (Stat box) -->
         @if(session('status'))
             <div class="alert alert-success">
                 {{ session('status') }}
@@ -44,91 +42,70 @@
                             </form>
                         </div>
                     </div>
-                    <!-- /.box-header -->
-                  <div class="box-body table-responsive no-padding">
-                    <table class="table table-hover">
-                        <thead>
-                            <tr>
-                                <th>STT</th>
-                                <th>Name</th>
-                                <th>Content</th>
-                                <th>Image</th>
-                                <th>Check Active</th>
-                                <th>Start Date</th>
-                                <th>End Date</th>
-                                <th>Created At</th>
-                                <th>Updated At</th>
-                                <th>Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @if($events->total() > 0)
-                                @php $count = 0; @endphp
-                                @foreach ($events as $event)
-                                    @php $count++; @endphp
-                                    <tr>
-                                        <td>{{ $count }}</td>
-                                        <td>{{ $event->name }}</td>
-                                        <td>{{ $event->content }}</td>
-                                        <td>
-                                            @if($event->image)
-                                                <img src="{{ asset('images/events/' . $event->image) }}" alt="{{ $event->name }}" width="100">
-                                            @else
-                                                No image
-                                            @endif
-                                        </td>
-                                        <td>{{ $event->check_active ? 'Active' : 'Inactive' }}</td>
-                                        <td>{{ $event->start_day }}</td>
-                                        <td>{{ $event->end_day }}</td>
-                                        <td>{{ $event->created_at }}</td>
-                                        <td>{{ $event->updated_at }}</td>
-                                        <td>
-                                            <a href="#" class="btn btn-xs btn-primary"><i class="fa fa-pencil"></i> Edit</a>
-                                            <form action="{{ route('deleteEvent', ['id' => $event->id_event]) }}" method="POST" style="display:inline;">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit" class="btn btn-xs btn-danger" onclick="return confirm('Bạn chắc chắn là xoá chứ?')">
-                                                    <i class="fa fa-trash"></i> Delete
-                                                </button>
-                                            </form>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            @else
+                    <div class="box-body table-responsive no-padding">
+                        <table class="table table-hover">
+                            <thead>
                                 <tr>
-                                    <td colspan="10" class="text-center">Rất tiếc, không có dữ liệu</td>
+                                    <th>STT</th>
+                                    <th>Name</th>
+                                    <th>Content</th>
+                                    <th>Image</th>
+                                    <th>Check Active</th>
+                                    <th>Start Date</th>
+                                    <th>End Date</th>
+                                    <th>Created At</th>
+                                    <th>Updated At</th>
+                                    <th>Action</th>
                                 </tr>
-                            @endif
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                @if($events->total() > 0)
+                                    @php $count = 0; @endphp
+                                    @foreach ($events as $event)
+                                        @php $count++; @endphp
+                                        <tr>
+                                            <td>{{ $count }}</td>
+                                            <td>{{ $event->name }}</td>
+                                            <td>{{ $event->content }}</td>
+                                            <td>
+                                                @if($event->image)
+                                                    <img src="{{ asset('images/events/' . $event->image) }}" alt="{{ $event->name }}" width="100">
+                                                @else
+                                                    No image
+                                                @endif
+                                            </td>
+                                            <td>{{ $event->check_active ? 'Active' : 'Inactive' }}</td>
+                                            <td>{{ $event->start_day }}</td>
+                                            <td>{{ $event->end_day }}</td>
+                                            <td>{{ $event->created_at }}</td>
+                                            <td>{{ $event->updated_at }}</td>
+                                            <td>
+                                                <a href="#" class="btn btn-xs btn-primary"><i class="fa fa-pencil"></i> Edit</a>
+                                                <form action="{{ route('deleteEvent', ['id' => $event->id_event]) }}" method="POST" style="display:inline;">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="btn btn-xs btn-danger" onclick="return confirm('Bạn chắc chắn là xoá chứ?')">
+                                                        <i class="fa fa-trash"></i> Delete
+                                                    </button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                @else
+                                    <tr>
+                                        <td colspan="10" class="text-center">Rất tiếc, không có dữ liệu</td>
+                                    </tr>
+                                @endif
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
-        <!-- Phân trang bắt đầu -->
+
+        <!-- Phân trang -->
         <div id="pageNavPosition" class="text-right">
-            <ul class="pagination">
-                {{-- @comment --}}
-
-                {{-- @if ($events->onFirstPage())
-                    <li class="disabled"><span>&laquo;</span></li>
-                @else
-                    <li><a href="{{ $events->previousPageUrl() }}" rel="prev">&laquo;</a></li>
-                @endif
-
-                @for ($i = 1; $i <= $events->lastPage(); $i++)
-                    <li class="{{ $i == $events->currentPage() ? 'active' : '' }}">
-                        <a href="{{ $events->url($i) }}">{{ $i }}</a>
-                    </li>
-                @endfor
-
-                @if ($events->hasMorePages())
-                    <li><a href="{{ $events->nextPageUrl() }}" rel="next">&raquo;</a></li>
-                @else
-                    <li class="disabled"><span>&raquo;</span></li>
-                @endif --}}
-            </ul>
+            {{ $events->links() }} 
         </div>
-        <!-- Phân trang kết thúc -->
     </section>
-    <!-- /.content -->
 @endsection
