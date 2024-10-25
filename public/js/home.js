13358ce332f632130b8bb269fec4899db7e91679
dangvanthua <thuandang021102@gmail.com>
2024-10-25 20:48:27 +0700

    fix some error in home.js

--- public/js/home.js
+++ public/js/home.js
@@ -1,5 +1,5 @@
-let currentPage = 1; // Khởi tạo trang hiện tại
-let currentCategoryId = 0; // Khởi tạo categoryId
+// let currentPage = 1; // Khởi tạo trang hiện tại
+// let currentCategoryId = 0; // Khởi tạo categoryId
 
 document.addEventListener('DOMContentLoaded', function () {
     console.log('Home js');
@@ -65,7 +65,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 <!-- Block2 -->
                 <div class="block2">
                     <div class="block2-pic hov-img0">
-                        <img src="../images/events/1729298479.png" alt="IMG-PRODUCT">
+                        <img src="${product.images}">
                         <a href="#" class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1">
                             Quick View
                         </a>
