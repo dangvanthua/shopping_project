cc9b73b6acecbf3723b79bd26ea76dc8854989af
CaoAnhVu141 <161787991+CaoAnhVu141@users.noreply.github.com>
2024-11-01 01:14:44 +0700

    commit by caoanhvu

--- app/Http/Controllers/Api/GetCartShoppingController.php
+++ app/Http/Controllers/Api/GetCartShoppingController.php
@@ -9,24 +9,6 @@ use Illuminate\Http\Request;
 class GetCartShoppingController extends Controller
 {
     //lấy sản phẩm trong giỏ hàng
-    // public function getItemsCartShopping(Request $request)
-    // {
-    //     // lấy id session
-    //     $id_session = session()->getId();
-    //     // lấy id_customer
-    //     $id_customer = auth()->check() ? auth()->id() : null;
-
-    //     $cartItems = ShoppingCart::with('product')->where(function($query) use ($id_session,$id_customer){
-    //         $query->where('id_session',$id_session);
-
-    //         if($id_customer)
-    //         {
-    //             $query->where('id_customer',$id_customer);
-    //         }
-    //     })->get();
-    //     return response()->json($cartItems);
-    // }
-
     public function getItemsCartShopping(Request $request)
     {
         $cartItems = null;
--- public/shopping/data_rest/shopping_cart.js
+++ public/shopping/data_rest/shopping_cart.js
@@ -62,11 +62,13 @@ document.addEventListener('DOMContentLoaded', function () {
         let cartContent = document.getElementById('items-cart');
         cartContent.innerHTML = '';
         let totalPrice = 0;
-        items.forEach(data => {
+        // chỉ cho in ra 3 sản phẩm thôi nè
+        const itemsCartShopping = items.slice(0,3);
+        itemsCartShopping.forEach(data => {
             totalPrice += parseFloat(data.total_price);
             const row = `<li class="header-cart-item flex-w flex-t m-b-12">
                 <div class="header-cart-item-img">
-                    <img src="images/item-cart-01.jpg" alt="IMG">
+                    <img src="{{asset("shoppimg/images/item-cart-01.jpg")}}" alt="IMG">
                 </div>
                 <div class="header-cart-item-txt p-t-8">
                     <a href="#" class="header-cart-item-name m-b-18 hov-cl1 trans-04">
