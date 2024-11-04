82a42098c73373fe7c8cb977ad0876bdfb8dcab2
dangvanthua <thuandang021102@gmail.com>
2024-11-04 20:32:02 +0700

    test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -86,9 +86,8 @@ class HomeController extends Controller
     public function loadMore(Request $request)
     {
         $categoryId = $request->input('category_id');
-        $page = $request->input('page', 1); // Lấy trang hiện tại, mặc định là 1
+        $page = $request->input('page', 1);
 
-        // Nếu không có categoryId, lấy tất cả sản phẩm
         if ($categoryId == 0) {
             $products = Product::paginate(8, ['*'], 'page', $page);
         } else {
