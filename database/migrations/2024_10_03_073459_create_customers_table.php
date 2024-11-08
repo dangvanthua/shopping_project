80e26323ad938db3632d6bbe0295ecb5e4a00adf
dangvanthua <thuandang021102@gmail.com>
2024-11-08 21:27:47 +0700

    fix error register

--- database/migrations/2024_10_03_073459_create_customers_table.php
+++ database/migrations/2024_10_03_073459_create_customers_table.php
@@ -17,7 +17,7 @@ return new class extends Migration {
             $table->string('email')->unique();
             $table->string('password');
             $table->string('phone');
-            $table->string('address')->nullable();
+            $table->string('address')->nullable()->default('');
             $table->timestamps();
         });
     }
