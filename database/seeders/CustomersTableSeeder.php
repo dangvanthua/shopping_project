73dceba849a542b1ba8f46a1138890149409e636
dangvanthua <thuandang021102@gmail.com>
2024-11-21 23:46:56 +0700

    fix my code after merge

--- database/seeders/CustomersTableSeeder.php
+++ database/seeders/CustomersTableSeeder.php
@@ -12,39 +12,39 @@ class CustomersTableSeeder extends Seeder
     public function run()
     {
         // Thêm khách hàng vào bảng customers
-        DB::table('customers')->insert(
+        DB::table('customers')->insert([
             [
                 'name' => 'Đỗ Tiến Đại',
                 'email' => 'dotiendai789@gmail.com',
                 'password' => bcrypt('password123'),
                 'phone' => '0123456789',
                 'address' => '123 Đường ABC, TP. Hồ Chí Minh',
-                'token' => strtoupper(Str::random(10)), // Không có token khi tạo mới
+                'token' => strtoupper(Str::random(10)),
             ],
             [
                 'name' => 'Lê Hoàng Thịnh',
                 'email' => 'le.hamthang21@gmail.com',
-                'password' => bcrypt('password123'), // Mật khẩu đã được mã hóa
+                'password' => bcrypt('password123'),
                 'phone' => '0123456789',
                 'address' => '123 Đường ABC, TP. Hồ Chí Minh',
-                'token' => strtoupper(Str::random(10)), // Không có token khi tạo mới
+                'token' => strtoupper(Str::random(10)),
             ],
             [
                 'name' => 'Trần Anh Tuấn',
                 'email' => 'anhtuan2551104@gmail.com',
-                'password' => bcrypt('password123'), // Mật khẩu đã được mã hóa
+                'password' => bcrypt('password123'),
                 'phone' => '0123456789',
                 'address' => '123 Đường ABC, TP. Hồ Chí Minh',
-                'token' => strtoupper(Str::random(10)), // Không có token khi tạo mới
+                'token' => strtoupper(Str::random(10)),
             ],
             [
-                'name' => 'Dang Van Thuan',
+                'name' => 'Đặng Văn Thuận',
                 'email' => 'thuandang021102@gmail.com',
-                'password' => bcrypt('password123'), // Mật khẩu đã được mã hóa
+                'password' => bcrypt('password123'),
                 'phone' => '0123456789',
                 'address' => '123 Đường ABC, TP. Hồ Chí Minh',
-                'token' => strtoupper(Str::random(10)), // Không có token khi tạo mới
-            ]
-        );
+                'token' => strtoupper(Str::random(10)),
+            ],
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -141,21 +141,23 @@ Route::get('/about', [AboutController::class, 'about'])->name('about');
 Route::get('/events', [EventController::class, 'index'])->name('events.index');
 Route::get('/events/create', [EventController::class, 'create'])->name('create');
 Route::post('/events/store', [EventController::class, 'store'])->name('events.store');
+Route::get('/events/{id}/edit', [EventController::class, 'edit'])->name('events.edit');
+Route::put('/events/{id}', [EventController::class, 'update'])->name('events.update');
 Route::delete('/events/{id}/delete', [EventController::class, 'destroy'])->name('deleteEvent');
+Route::get('/events/search', [EventController::class, 'search'])->name('events.search');
 
 // Route Review
-// Hàm này mục đích chỉ để hiển thị trang chi tiết sản phẩm sẽ bị thay thế @note lại
-// Route::get('/product-detail', [ReviewController::class, 'index'])->name('product_detail');
-// Route::post('/submit-review', [ReviewController::class, 'saveReview']);
-// Route::delete('/reviews/{id}', [ReviewController::class, 'removeReview']);
-// Route::put('/reviews/{id}', [ReviewController::class, 'updateReview']);
+Route::get('/product-detail/{id_product}', [ReviewController::class, 'index'])->name('product_detail');
+Route::post('/submit-review', [ReviewController::class, 'saveReview']);
+Route::delete('/reviews/{id}', [ReviewController::class, 'removeReview']);
+Route::put('/reviews/{id}', [ReviewController::class, 'updateReview']);
+
 
 // Route User
 Route::get('/auth/forgot_password', [UserController::class, 'showForgotPassword'])->name('auth.password');
 Route::post('/auth/forgot_password', [UserController::class, 'submitFormForgetPassword'])->name('auth.sumitReset');
 Route::get('/auth/get_password/{customer}/{token}', [UserController::class, 'showGetPassword'])->name('auth.getPassword');
 Route::post('/auth/get_password/{customer}/{token}', [UserController::class, 'submitGetPassword'])->name('auth.submitPassword');
-
 
 
 //Của Thịnh
@@ -199,8 +201,6 @@ Route::post('/add-categorypost', [CategoryPostViewController::class, 'addDataCat
 Route::get('/delete-category-post/{id}', [CategoryPostViewController::class, 'deleteDataCategoryPost'])->name('delete-category-post');
 Route::get('/update-category-post/{id}', [CategoryPostViewController::class, 'showUpdateDataCategoryPost'])->name('update-category-post');
 Route::post('/update-category-post/{id}', [CategoryPostViewController::class, 'UpdateDataCategoryPost'])->name('updatecategorypost');
-
-
 
 
 Route::get('/attibute', [AttributeViewController::class, 'showThemmeAttributeIndex']);
